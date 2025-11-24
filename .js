7474e5c9fa99c199e98ba10afab8a2daaf59e472
Juanse-34 <sanchezriverajuansebastian1@gmail.com>
2025-11-24 16:28:17 -0500

    Agregar estructura base de la rama feature/footer

---
+++ .js
@@ -0,0 +1,17 @@
+/*
+  Rama: feature/footer
+  Fecha: 24/11/25
+  Descripción: Renderizar el footer de la aplicación.
+*/
+
+function renderFooter() {
+  const footer = `
+    -------------------------
+        Mi App © 2025
+    -------------------------
+  `;
+
+  console.log(footer);
+}
+
+renderFooter();
